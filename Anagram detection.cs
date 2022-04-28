3234b24e277a6f3726b6e48e2de2df1c5c4a7dbe
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-28 10:53:05 +0530

    push Anagram

---
+++ Anagram detection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+    public class Anagram_detection
+    {
+        public static void main()
+        {
+            string str1 = "abcd";
+            string str2 = "dcba";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine(" Both the strings are Anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both the string are not Anagrams");
+            }
+        }
+    }
+
